7fbe5f45b8653bdb50ca2aa279c7b8eef4100380
Davi Zanotto <davizanotto@MacBook-Pro-de-Davi-2.local>
2017-12-22 14:57:32 -0300

    Environment configured

---
+++ src/containers/app/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom'
+import Home from '../home'
+import About from '../about'
+
+export default props => (
+  <div>
+    <header>
+      <Link to="/">Home</Link>&nbsp;|&nbsp;
+      <Link to="/about-us">About</Link>
+    </header>
+
+    <main>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about-us" component={About} />
+    </main>
+  </div>
+)
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import store, { history } from './store'
+import App from './containers/app'
+
+import './index.css'
+
+const target = document.querySelector('#root')
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <div>
+                <App />
+            </div>
+        </ConnectedRouter>
+    </Provider>,
+    target
+);
+
+registerServiceWorker();
